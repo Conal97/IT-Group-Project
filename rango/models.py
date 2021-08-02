298c9a229ac25f7b19762d09daee8fb3cdc92929
WilliamC21 <84275546+WilliamC21@users.noreply.github.com>
2021-08-02 16:06:17 +0900

    minor changes to wording in some templates

--- rango/models.py
+++ rango/models.py
@@ -3,8 +3,10 @@ from django.db.models.deletion import CASCADE
 from django.template.defaultfilters import slugify
 from django.contrib.auth.models import User
 
+Max_Length = 128
+
 class Area(models.Model):
-    name = models.CharField(max_length=128, unique = True)
+    name = models.CharField(max_length=Max_Length, unique = True)
 
     def save(self, *args, **kwargs):
         self.slug = slugify(self.name)
@@ -17,12 +19,12 @@ class Area(models.Model):
         return self.name
 
 class Munro(models.Model):
-    name = models.CharField(max_length = 128, unique=True)
-    area = models.CharField(max_length = 128, default = "")
+    name = models.CharField(max_length = Max_Length, unique=True)
+    area = models.CharField(max_length = Max_Length, default = "")
     picture = models.ImageField(upload_to='Munro_Images', blank=True)
     difficulty = models.IntegerField(default = 0)
     elevation = models.IntegerField(default = 0)
-    coordinates = models.CharField(default = "")
+    coordinates = models.CharField(max_length = Max_Length, default = "")
     area = models.ForeignKey(Area, on_delete=models.CASCADE)
 
     def save(self, *args, **kwargs):
@@ -37,7 +39,7 @@ class Munro(models.Model):
         
 class Hiker(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    bagged = models.CharField(default = "")
+    bagged = models.CharField(max_length = Max_Length, default = "")
     picture = models.ImageField(upload_to='profile_images', blank=True)
     verified = models.BooleanField(default=False)
 
@@ -57,3 +59,42 @@ class Report(models.Model):
 
     def __str__(self):
         return self.munro.name
+
+class Category(models.Model):
+    NAME_MAX_LENGTH =128
+
+    name = models.CharField(max_length=NAME_MAX_LENGTH, unique=True)
+    views = models.IntegerField(default=0)
+    likes = models.IntegerField(default=0)
+    slug = models.SlugField(unique = True)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Category, self).save(*args, **kwargs)
+
+    class Meta:
+        verbose_name_plural='Categories'
+
+    def __str__(self):
+        return self.name
+
+class Page (models.Model):
+    TITLE_MAX_LENGTH = 128
+    URL_MAX_LENGTH = 200
+
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    title = models.CharField(max_length = TITLE_MAX_LENGTH)
+    url = models.URLField()
+    views = models.IntegerField(default=0)
+    
+    def __str__(self):
+        return self.title
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    website = models.URLField(blank=True)
+    picture = models.ImageField(upload_to='profile_images', blank=True)
+
+    def __str__(self):
+        return self.user.username
