6b1b55173c12bc6412b3ffd69af54af76d9a4556
WilliamC21 <84275546+WilliamC21@users.noreply.github.com>
2021-08-02 13:58:45 +0900

    models first draft

--- rango/models.py
+++ rango/models.py
@@ -1,42 +1,42 @@
 from django.db import models
+from django.db.models.deletion import CASCADE
 from django.template.defaultfilters import slugify
 from django.contrib.auth.models import User
 
-class Category(models.Model):
-    NAME_MAX_LENGTH =128
+class Area(models.Model):
+    name = models.CharField(max_length=128, unique = True)
 
-    name = models.CharField(max_length=NAME_MAX_LENGTH, unique=True)
-    views = models.IntegerField(default=0)
-    likes = models.IntegerField(default=0)
-    slug = models.SlugField(unique = True)
+class Munro(models.Model):
+    name = models.CharField(max_length = 128, unique=True)
+    area = models.CharField(max_length = 128, default = "")
+    picture = models.ImageField(upload_to='Munro_Images', blank=True)
+    difficulty = models.IntegerField(default = 0)
+    elevation = models.IntegerField(default = 0)
+    coordinates = models.CharField(default = "")
+    area = models.ForeignKey(Area, on_delete=models.CASCADE)
 
     def save(self, *args, **kwargs):
         self.slug = slugify(self.name)
-        super(Category, self).save(*args, **kwargs)
-
+        super(Munro, self).save(*args, **kwargs)
+    
     class Meta:
-        verbose_name_plural='Categories'
+        verbose_name_plural='Munros'
 
     def __str__(self):
         return self.name
-
-class Page (models.Model):
-    TITLE_MAX_LENGTH = 128
-    URL_MAX_LENGTH = 200
-
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    title = models.CharField(max_length = TITLE_MAX_LENGTH)
-    url = models.URLField()
-    views = models.IntegerField(default=0)
-    
-    def __str__(self):
-        return self.title
-
-class UserProfile(models.Model):
+        
+class Hiker(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-
-    website = models.URLField(blank=True)
+    bagged = models.CharField(default = "")
     picture = models.ImageField(upload_to='profile_images', blank=True)
+    verified = models.BooleanField(default=False)
 
     def __str__(self):
         return self.user.username
+
+class Report(models.Model):
+    author = models.OneToOneField(Hiker, on_delete=CASCADE)
+    munro = models.ForeignKey(Munro, on_delete=models.CASCADE)
+    picture = models.ImageField(upload_to ='report_images', blank = True)
+    difficulty = models.IntegerField(default = 0)
+    report_text = models.CharField
