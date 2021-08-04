5e23715ef47834fdf478b132d72e01e40c9a3c98
eddyStace <estace@live.com>
2021-08-04 10:26:44 +0100

    removed unnecessary views.py

--- rango/views.py
+++ rango/views.py
@@ -12,6 +12,45 @@ from django.contrib.auth.decorators import login_required
 from datetime import datetime
 from django.utils import timezone
 
+def index(request):
+    #dictionary used to pass into template as context()
+    category_list = Category.objects.order_by('-likes')[:5]
+    pages_list = Page.objects.order_by('-views')[:5]
+
+    context_dict = {}
+    context_dict['pageheading'] = 'Rango'
+    context_dict['categories'] = category_list
+    context_dict['pages'] = pages_list
+    
+    visitor_cookie_handler(request)
+
+    response = render(request, 'rango/index.html', context=context_dict)
+    return response
+
+def about(request):
+    context_dict = {'pageheading': 'About Rango'}
+    return render(request, 'rango/about.html', context=context_dict)
+
+def search(request):
+    result_list = []
+
+    if request.method == 'POST':
+        query = request.POST['query'].strip()
+
+        if query:
+            # Use the bing search function
+            result_list = run_query(query)
+
+    return render(request, 'rango/search.html', {'result_list':result_list})
+
+def photo_gallery(request):
+    context_dict = {'pageheading': 'Photo Gallery'}
+    return render(request, 'rango/photo_gallery.html', context = context_dict)
+
+@login_required
+def restricted(request):
+    return render(request, 'rango/restricted.html')
+
 def munro(request, munro_name_slug):
     context_dict = {}
     context_dict['details'] = "Details: "
@@ -60,13 +99,6 @@ def show_area(request, area_name_slug):
     return render(request, 'rango/show_area.html', context=context_dict)
 
 #helper func
-def get_server_side_cookie(request, cookie, default_val=None):
-    val=request.session.get(cookie)
-    if not val:
-        val = default_val
-    return val
-
-#helper func
 def visitor_cookie_handler(request):
     visits = int(get_server_side_cookie(request, 'visits', '1'))
     last_visit_cookie = get_server_side_cookie(request, 'last_visit', str(datetime.now()))
@@ -80,113 +112,12 @@ def visitor_cookie_handler(request):
 
     request.session['visits'] = visits
 
-def index(request):
-    #dictionary used to pass into template as context()
-    category_list = Category.objects.order_by('-likes')[:5]
-    pages_list = Page.objects.order_by('-views')[:5]
-
-    context_dict = {}
-    context_dict['pageheading'] = 'Rango'
-    context_dict['categories'] = category_list
-    context_dict['pages'] = pages_list
-    
-    visitor_cookie_handler(request)
-
-    response = render(request, 'rango/index.html', context=context_dict)
-    return response
-
-def about(request):
-    context_dict = {'pageheading': 'About Rango'}
-    return render(request, 'rango/about.html', context=context_dict)
-
-def search(request):
-    result_list = []
-
-    if request.method == 'POST':
-        query = request.POST['query'].strip()
-
-        if query:
-            # Use the bing search function
-            result_list = run_query(query)
-
-    return render(request, 'rango/search.html', {'result_list':result_list})
-
-
-def show_category(request, category_name_slug):
-    #create a context dictionary(will be passed to html file)
-    context_dict = {}
-
-    try:
-        #try to find category name slug with given name(prameter passed)
-        category = Category.objects.get(slug=category_name_slug)
-
-        #retrieve all pages associated
-        pages = Page.objects.filter(category = category)
-        
-        #add these pages to the dictionary
-        context_dict['pages'] = pages
-
-        #this used to verify category exists
-        context_dict['category'] = category
-    except Category.DoesNotExist: #throw exception if no such category
-        #avoid doing anything, disaply no category message
-        context_dict['category'] = None
-        context_dict['pages'] = None
-    
-    return render(request, 'rango/category.html', context=context_dict)
-
-@login_required
-def add_category(request):
-    form = CategoryForm()
-
-    if request.method == 'POST':
-        form = CategoryForm(request.POST)
-
-        if form.is_valid():
-            form.save(commit=True)
-            return redirect(reverse('rango:index'))
-        
-        else: print (form.errors)
-
-    return render(request, 'rango/add_category.html', {'form': form})
-
-@login_required
-def add_page(request, category_name_slug):
-    try:
-        category = Category.objects.get(slug=category_name_slug)
-    except:
-        category = None
-
-    if category is None:
-        return redirect(reverse('rango:index'))
-
-    form = PageForm()
-
-    if request.method == 'POST':
-        form = PageForm(request.POST)
-
-        if form.is_valid():
-            if category:
-                page = form.save(commit=False)
-                page.category = category
-                page.views = 0
-                page.save()
-                print("HERE")
-                return redirect(reverse('rango:show_category', kwargs={'category_name_slug': category_name_slug}))
-            else:
-                print(form.error)
-
-    context_dict = {'form' : form, 'category': category}
-
-    return render(request, 'rango/add_page.html', context = context_dict)
-
-@login_required
-def restricted(request):
-    return render(request, 'rango/restricted.html')
-
-def photo_gallery(request):
-    context_dict = {'pageheading': 'Photo Gallery'}
-    return render(request, 'rango/photo_gallery.html', context = context_dict)
+#helper func
+def get_server_side_cookie(request, cookie, default_val=None):
+    val=request.session.get(cookie)
+    if not val:
+        val = default_val
+    return val
 
 '''def register(request):
 
