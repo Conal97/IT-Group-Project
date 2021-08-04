e61ce804411576def15678b74c3a31b4cc3425da
WilliamC21 <84275546+WilliamC21@users.noreply.github.com>
2021-08-04 23:33:12 +0900

    adding generic desc to munros, adding desc to index xx

--- populate_rango.py
+++ populate_rango.py
@@ -11,14 +11,16 @@ def populate():
         {'name' : 'Ben Nevis',
         'difficulty' : 1,
         'elevation': 1345,
-        'coordinates': '56.7969° N, 5.0036° W'},
+        'coordinates': '56.7969° N, 5.0036° W',
+        'description': 'Generic description',}
     ]
 
     cairngorms_munros = [
         { 'name' : 'Ben Macdui',
         'difficulty' : 2,
         'elevation': 1309,
-        'coordinates': '57.0704° N, 3.6691° W'},
+        'coordinates': '57.0704° N, 3.6691° W',
+        'description': 'Generic description',}
 
     ]
 
@@ -26,12 +28,14 @@ def populate():
         {'name' : 'Ben Lomond',
         'difficulty' : 5,
         'elevation': 974,
-        'coordinates': '56.7969° N, 5.0036° W'},
+        'coordinates': '56.7969° N, 5.0036° W',
+        'description': 'Generic description',},
 
         {'name' : 'Ben Vane',
         'difficulty' : 4,
         'elevation': 915,
-        'coordinates': '56.2499° N, 4.7817° W'},
+        'coordinates': '56.2499° N, 4.7817° W',
+        'description': 'Generic description',}
     ]
 
     areas = {'Fort William' : {'munros': fort_william_munros}, 
@@ -42,17 +46,18 @@ def populate():
     for area, area_data in areas.items():
         a = add_area(area)
         for m in area_data['munros']:
-            add_munro(a, m['name'], m['difficulty'], m['elevation'], m['coordinates'])
+            add_munro(a, m['name'], m['difficulty'], m['elevation'], m['coordinates'], m['description'])
 
     for a in Area.objects.all():
         for m in Munro.objects.filter(area=a):
             print(f'- {a}: {m}')
 
-def add_munro(area, name, diff, ele, coords):
+def add_munro(area, name, diff, ele, coords, desc):
     m = Munro.objects.get_or_create(area = area, name=name)[0] 
     m.difficulty = diff
     m.elevation = ele
     m.coordinates = coords
+    m.description = desc
     m.save()
     return m
 
