86638293bbaac0e4400738913aef04a5610ad12b
eddyStace <estace@live.com>
2021-08-04 10:23:35 +0100

    removed unwanted html files

--- rango/urls.py
+++ rango/urls.py
@@ -12,7 +12,6 @@ urlpatterns = [
     path('restricted/', views.restricted, name='restricted'),
     path('areas/', views.area, name='areas'),
     path('area/<slug:area_name_slug>/', views.show_area, name='show_area'),
-    path('post-report', views.hike_report, name='hike_report'),
     path('search', views.search, name='search'),
     path('photo_gallery/', views.photo_gallery, name='photo_gallery'),
 ]
