3b81df846be8e80218252268ef3eb25aca8646a1
WilliamC21 <84275546+WilliamC21@users.noreply.github.com>
2021-08-06 23:36:32 +0900

    removing userprofile obj from models

--- rango/models.py
+++ rango/models.py
@@ -138,12 +138,3 @@ class Report(models.Model): #text that user uploads about a munro
 
     def __str__(self):
         return self.munro.name
-
-class UserProfile(models.Model): 
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-
-    website = models.URLField(blank=True)
-    picture = models.ImageField(upload_to='profile_images', blank=True)
-
-    def __str__(self):
-        return self.user.username
